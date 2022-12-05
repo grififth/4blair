b4a0059b72115f90bdcce3bac22063765e51d0b7
grififth <stevez9101@gmail.com>
2022-12-05 09:16:17 -0500

    break-all

--- components/Comment.tsx
+++ components/Comment.tsx
@@ -16,7 +16,9 @@ const Comment = ({ comment, setReplyToId, newCommentRef }: CommentProps) => {
           {comment["replyToContent"]}
         </h2>
       )}
-      <p className="text-lg whitespace-pre-wrap">{comment["content"]}</p>
+      <p className="text-lg whitespace-pre-wrap break-all">
+        {comment["content"]}
+      </p>
       <div className="flex justify-between text-sm">
         <p>{timeDiff(new Date(Date.parse(comment.createdAt)))}</p>
         <button
